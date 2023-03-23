347e84b59d0b4e12f0adcef2cb2dcbc1e281577c
aigerim055 <omurzakova727@gmail.com>
2023-03-23 22:22:19 +0600

    calculator

--- script.js
+++ script.js
@@ -7,29 +7,116 @@ const buttonMinus = document.querySelector('.minus')
 const buttonMultiplication = document.querySelector('.multiplication')
 const buttonDivision = document.querySelector('.division')
 const res = document.querySelector('#result')
+const NumbersButton = document.querySelectorAll('[data-button]')
+const buttonReset = document.querySelector('.btn-reset')
+const buttonEqual = document.querySelector('.btn-equal')
+const buttonClear = document.querySelector('.btn-clear')
 
 
-let num = 0
+let num = "0"
 res.innerText = num
 
-buttonPlus.addEventListener('click', () => {
-    if (num < 10)  res.innerText = num += 1
-    buttonPlus.disabled = true
+buttonClear.addEventListener('click', () => {
+    num = '0'
+    res.innerText = num
+    buttonPlus.disabled = false
+    buttonMinus.disabled = false
 })
 
-buttonMinus.addEventListener('click', () => {
-    if (num > 0) {
-        res.innerText = num -= 1
-        buttonMinus.disabled = false
+
+NumbersButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (num === '0') {
+            res.innerText = button.innerText
+        } else {
+            res.innerText = res.innerText +  button.innerText
+        }
+        num = res.innerText
+    })
+})
+
+buttonReset.addEventListener('click', () => {
+    if (num !== '0'){
+        if (num.length > 1){
+            num = num.slice(0, -1)
+            res.innerText = num
+        } else {
+            num = '0'
+            res.innerText = num
+            buttonPlus.disabled = false
+            buttonMinus.disabled = false
+        }
     }
 })
 
-buttonMultiplication.addEventListener('click', () => {
-    num *= num
-    res.innerText = num
+
+buttonEqual.addEventListener('click', () => {
+    if (res.innerText.includes('-')){
+        let a = Number(res.innerText.slice(0, res.innerText.indexOf('-')))
+        let b = Number(res.innerText.slice(res.innerText.indexOf('-')+1, res.innerText.length))
+        res.innerText = a - b
+    } else if (res.innerText.includes('+')){
+        let a = Number(res.innerText.slice(0, res.innerText.indexOf('+')))
+        let b = Number(res.innerText.slice(res.innerText.indexOf('+'), res.innerText.length))
+        res.innerText = a + b
+    } else if (res.innerText.includes('*')){
+        let a = Number(res.innerText.slice(0, res.innerText.indexOf('*')))
+        let b = Number(res.innerText.slice(res.innerText.indexOf('*')+1, res.innerText.length))
+        res.innerText = a * b
+    } else if (res.innerText.includes('/')){
+        let a = Number(res.innerText.slice(0, res.innerText.indexOf('/')))
+        let b = Number(res.innerText.slice(res.innerText.indexOf('/')+1, res.innerText.length))
+        res.innerText = a / b
+    }
+
 })
 
-buttonDivision.addEventListener('click', () => {
-    num /= 2
-    res.innerText = num
+
+
+buttonPlus.addEventListener('click', () => {
+    if (res.innerText.includes('+')){
+        let a = Number(res.innerText.slice(0, res.innerText.indexOf('+')))
+        let b = Number(res.innerText.slice(res.innerText.indexOf('+'), res.innerText.length))
+        res.innerText = a + b
+    } else {
+        res.innerText = res.innerText + buttonPlus.innerText
+    }
 })
+
+buttonMinus.addEventListener('click', () => {
+    if (res.innerText.includes('-')){
+        let a = Number(res.innerText.slice(0, res.innerText.indexOf('-')))
+        let b = Number(res.innerText.slice(res.innerText.indexOf('-'), res.innerText.length))
+        res.innerText = `${a}-${b}`
+    } else {
+        res.innerText = `${res.innerText}${buttonMinus.innerText}`
+    }
+})
+
+
+buttonMultiplication.addEventListener('click', () => {
+    if (res.innerText.includes('*')){
+        let a = Number(res.innerText.slice(0, res.innerText.indexOf('*')))
+        let b = Number(res.innerText.slice(res.innerText.indexOf('*'), res.innerText.length))
+        res.innerText = `${a}*${b}`
+    } else {
+        res.innerText = `${res.innerText}${buttonMultiplication.innerText}`
+    }
+})
+
+
+
+buttonDivision.addEventListener('click', () => {
+    if (res.innerText.includes('/')){
+        let a = Number(res.innerText.slice(0, res.innerText.indexOf('/')))
+        let b = Number(res.innerText.slice(res.innerText.indexOf('/'), res.innerText.length))
+        res.innerText = `${a}/${b}`
+    } else {
+        res.innerText = `${res.innerText}${buttonDivision.innerText}`
+    }
+})
+
+
+
+
+// TODO: fix minus, umnojenie - если сразу два минуса ломается 9--9
